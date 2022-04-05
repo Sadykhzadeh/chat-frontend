eed16575c0cb4c9f92683f12c15d29564fbdfc31
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-04-05 09:13:53 +0400

    Improved 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,17 @@
+import { Container, Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const err: NextPage = () => {
   return (
-    <div>
-      <h1>404</h1>
-    </div>
+    <Container component="main">
+      <Typography component="h1" variant="h4">
+        No way, 404! :0
+      </Typography>
+      <Link href="/">
+        Go to home page
+      </Link>
+    </Container>
   )
 }
 
