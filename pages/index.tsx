45c6bb29122557356747b6d03ee7a9c61c5639cb
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-04-24 01:04:17 +0400

    remove useless comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,27 +10,4 @@ const Home: NextPage = () => {
   )
 }
 
-// export const getServerSideProps = async (ctx) => {
-//   const cookies = nookies.get(ctx)
-//   if (!cookies.token) {
-//     return {
-//       props: {}
-//     };
-//   }
-//   const token = cookies.token
-//   console.log(ctx.resolvedUrl);
-
-//   const res = await axios.get(`http://0.0.0.0:3000/api/me`, {
-//     headers: {
-//       Authorization: `Bearer ${token}`
-//     }
-//   })
-//   const user = res.data as UserResponse
-//   return {
-//     props: {
-//       user
-//     }
-//   }
-// }
-
 export default Home
