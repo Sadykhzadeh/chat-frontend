f876b1b178ad095a02e4d89828158abdb07fe1f8
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-04-24 01:03:21 +0400

    add getCookie for onClick handling

---
+++ src/getCookie.ts
@@ -0,0 +1,6 @@
+export const getCookie = (header: string, name: string): string | undefined => {
+  let matches = header.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
