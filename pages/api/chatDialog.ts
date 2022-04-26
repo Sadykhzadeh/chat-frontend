d929c936dc08d925aafbcdf5a7f280ec778466e9
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-04-26 11:30:12 +0400

    Update message time displaying

--- pages/api/chatDialog.ts
+++ pages/api/chatDialog.ts
@@ -53,15 +53,7 @@ const chatDialog = async (req: NextApiRequest, res: NextApiResponse) => {
         })
         // messageData.content = decrypt(messageData.content, req.headers.dec as string)
         const creationTime = new Date(messageData.creationTime);
-        messageData.creationTime = creationTime.toLocaleString('en-US', {
-          hour12: false,
-          hour: 'numeric',
-          minute: 'numeric',
-          second: 'numeric',
-          year: 'numeric',
-          month: 'numeric',
-          day: 'numeric',
-        });
+        messageData.creationTime = creationTime.toLocaleString();
         messagesList.push(messageData);
       }
 
