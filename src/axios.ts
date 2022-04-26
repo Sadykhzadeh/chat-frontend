663330531cb6daa11f74a942e7a40699026854ec
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-04-26 09:45:25 +0400

    Increase axios timeout

--- src/axios.ts
+++ src/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // axios is a promise based HTTP client for the browser and node.js
 const mainServer = axios.create({
   baseURL: 'http://64.227.78.16:8081/chat/',
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
