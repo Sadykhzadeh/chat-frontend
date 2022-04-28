59c7d41d0ab3a498e5d32ad24f4e031340d6d11d
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-04-28 11:25:26 +0400

    Add "Edit profile" in main page

--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -2,15 +2,19 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import Avatar from '@mui/material/Avatar';
+import Link from 'next/link';
+
 
 const ProfileInfo = ({ user }) => <Card sx={{
   mt: 4
 }}>
   <CardHeader
     avatar={
-      <Avatar aria-label="recipe">
-        {(user?.name[0].toUpperCase() + user?.surname[0].toUpperCase())}
-      </Avatar>
+      <Link href={'/chat/update'} passHref>
+        <Avatar aria-label="recipe">
+          {(user?.name[0].toUpperCase() + user?.surname[0].toUpperCase())}
+        </Avatar>
+      </Link>
     }
     title={user?.name + ' ' + user?.surname + ' (ID: ' + user?.id + ')'}
     subheader={user?.login}
